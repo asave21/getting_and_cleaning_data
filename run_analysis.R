2c496ff6346eb171ab86986a5fe04e7313464376
Ankur Save <asave21@gmail.com>
2014-04-27 17:23:58 +0530

    commit 2
    3 parts done

--- run_analysis.R
+++ run_analysis.R
@@ -9,8 +9,11 @@ act_labels <- read.table("~/UCI_HAR_Dataset/activity_labels.txt", header = FALSE
 act<- merge(act,act_labels)
 col <- c("Activity_ID", "Activity_Name")
 colnames(act)<-col
+act_final <- data.frame(act$Activity_Name)
+
+#Appropriately labels the data set with descriptive activity names. 
+val_labels <- read.table("~/UCI_HAR_Dataset/features.txt", header = FALSE)
+colnames(val) <- val_labels$vs
 
 #Extracts only the measurements on the mean and standard deviation for each measurement. 
-#Uses descriptive activity names to name the activities in the data set
-#Appropriately labels the data set with descriptive activity names. 
 #Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
